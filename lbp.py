13bd62447c39421fa0f8dd9092b222867abf1dcb
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-02-20 18:23:37 +0700

    Nambah dikit

--- lbp.py
+++ lbp.py
@@ -1,9 +1,13 @@
 import numpy as np
 
 
-def neigh():
-    pass
+def lbp(img):
+    if img.ndim != 2:
+        print('Citra harus berformat grayscale')
+
+    neigh = np.array(((-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, -1)))
+    multiply = 1, 2, 4, 8, 16, 32, 64, 128
 
 
-def lbp():
-    pass
+if __name__ == '__main__':
+    lbp()
