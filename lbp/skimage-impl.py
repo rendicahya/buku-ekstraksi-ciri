b5d7be86934841075813fdf2d2b9dcb2047c21d5
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-03-11 18:31:09 +0700

    A small modification

--- lbp/skimage-impl.py
+++ lbp/skimage-impl.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 from skimage import feature
 
-img = cv2.imread('teddy.jpg', 0)
+img = cv2.imread('../teddy.jpg', 0)
 lbp = feature.local_binary_pattern(img, 8, 1)
-stack = np.vstack((img, lbp.astype(np.uint8)))
 
-cv2.imshow('LBP', stack)
+cv2.imshow('Citra', img)
+cv2.imshow('LBP', lbp.astype(np.uint8))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
