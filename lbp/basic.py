2036b4140a5f36bd1b896b64e1b7f85f1f3e2b85
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-03-14 21:23:42 +0700

    Basic LBP

--- lbp/basic.py
+++ lbp/basic.py
@@ -4,6 +4,7 @@ from skimage import feature
 
 
 def lbp(img):
+    img = np.pad(img, 1, 'constant', constant_values=0)
     neighbors = np.array(((-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)))
     multiply = 1, 2, 4, 8, 16, 32, 64, 128
     h, w = img.shape[:2]
@@ -19,23 +20,26 @@ def lbp(img):
                 # print('%d : %d = %d' % (img[y, x], img[Y, X], bin[i]))
 
             output[Y - 1, X - 1] = np.dot(bin, multiply)
-            print(bin)
+            # print('{} {} {}'.format(img[Y,X], bin, output[Y - 1, X - 1]))
 
-    return np.pad(output, 1, 'constant')
+    return output
 
 
 if __name__ == '__main__':
     # img = cv2.imread('../teddy.jpg', 0)
-    img = np.array([[118, 98, 112, 104],
-                    [105, 80, 111, 101],
-                    [64, 90, 94, 107],
-                    [57, 84, 101, 90]])
+    img = np.array([[120, 180, 190, 170],
+                    [110, 150, 240, 220],
+                    [100, 140, 210, 180],
+                    [120, 160, 130, 240]])
+
+    print(img)
 
     lbp1 = lbp(img)
-    lbp2 = feature.local_binary_pattern(img, 8, 1)
-
     print(lbp1)
+
+    lbp2 = feature.local_binary_pattern(img, 8, 1)
     print(lbp2)
+
     # cv2.imshow('Diva', img)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
